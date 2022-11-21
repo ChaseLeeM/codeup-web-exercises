40b22cf01bce01ec7d59ddf153b4130f84ab851f
Chase Medford <chasemedford1@gmail.com>
2022-11-21 13:37:56 -0600

    New external, internal, and external.js files.

---
+++ js/external.js
@@ -0,0 +1,9 @@
+console.log("Welcome to External JavaScript");
+
+alert("Welcome to my Website!");
+
+let favColor = prompt("Whats your favorite color?");
+
+let message = "Great, " + favColor + " is my favorite color too!";
+
+alert(message);
