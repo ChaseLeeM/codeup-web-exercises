a4176d9712deb1dc9b0f2019c13c81928c5f9251
Chase Medford <chasemedford1@gmail.com>
2023-01-21 23:12:39 -0600

    Mapbox Needs More work

---
+++ js/mapbox-geocoder-utils.js
@@ -0,0 +1,11 @@
+function geocode(search, token) {
+    let baseUrl = 'https://api.mapbox.com';
+    let endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+            // to get all the data from the request, comment out the following three lines...
+        }).then(function(data) {
+            return data.features[0].center;
+        });
+}
