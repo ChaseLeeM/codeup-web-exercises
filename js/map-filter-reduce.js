d0a5fbca1c547f72e68ffafa0bff9adc30f7a92f
Chase Medford <chasemedford1@gmail.com>
2023-01-26 16:46:38 -0600

    Map works. Was missing a comma in keys.js. !!!

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,65 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+const threeLang = users.filter(n => n.languages.length >= 3);
+console.log(threeLang);
+
+const emails = users.map(n => n.email);
+console.log(emails);
+
+const totalYears = users.reduce((total, user) => {
+    return total + user.yearsOfExperience / users.length;
+}, 0);
+console.log(totalYears);
+
+const longestEmail = users.reduce((longest, user) => {
+    if (user.email.length > longest.length) {
+        return user.email;
+    } else {
+        return longest;
+    }
+}, "");
+console.log(longestEmail);
+
+
+const instructors = users.reduce((names, user) => {
+    return names + user.name + ", ";
+}, "Your instructors are: ");
+console.log(instructors);
+
+
